20f7647cc76131085a3e67b21245fc0cbd39e8d7
chetankondlewar <chetan.kondlewar@nciportal.com>
2023-01-07 22:18:12 +0530

    def

---
+++ Test.java
@@ -0,0 +1,19 @@
+package com.GitDemo.demo;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=12;
+		
+		System.out.println(a+b);
+		 String s="chetan";
+		 
+		 if(s=="chetan")
+			 System.out.println("Yes");
+		 else
+			 System.out.println("No");
+	}
+	
+}
